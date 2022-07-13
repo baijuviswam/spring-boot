6aafa79e6e0d96956802fe58387870b01f225f5f
4702labinc <4702labinc@localhost>
2022-07-13 15:54:41 -0700

    logger updated

--- spring-aws-storage-blob/src/main/java/com/lab/aws/storage/SpringAwsStorageBlobApplication.java
+++ spring-aws-storage-blob/src/main/java/com/lab/aws/storage/SpringAwsStorageBlobApplication.java
@@ -1,5 +1,7 @@
 package com.lab.aws.storage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -10,6 +12,7 @@ import com.amazonaws.services.s3.model.Bucket;
 
 @SpringBootApplication
 public class SpringAwsStorageBlobApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringAwsStorageBlobApplication.class);
 	private final AmazonS3Client amazonS3Client;
 
 	public static void main(String[] args) {
@@ -23,7 +26,7 @@ public class SpringAwsStorageBlobApplication {
 	@EventListener(classes = ApplicationReadyEvent.class)
 	public void onApplicationReadyEvent(ApplicationReadyEvent event) {
 		for (Bucket availableBuckets : amazonS3Client.listBuckets()) {
-			System.out.println(availableBuckets.getName());
+			logger.info(availableBuckets.getName());
 		}
 	}
 
